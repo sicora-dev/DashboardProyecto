10a4cfd3b64478b6d624906976cc1807ab00d1ce
sicora-dev <sirocornejoraez@gmail.com>
2025-02-24 17:52:01 +0100

    asdasd

--- ViewModels/InfoCard.xaml.cs
+++ ViewModels/InfoCard.xaml.cs
@@ -24,5 +24,37 @@ namespace DashboardTienda.UserControls
         {
             InitializeComponent();
         }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty = 
+            DependencyProperty.Register("Title", typeof(string), typeof(InfoCard));
+
+        public string SubTitle
+        {
+            get { return (string)GetValue(SubTitleProperty); }
+            set { SetValue(SubTitleProperty, value); }
+        }
+        public static readonly DependencyProperty SubTitleProperty =
+            DependencyProperty.Register("SubTitle", typeof(string), typeof(InfoCard));
+
+        public string Amount
+        {
+            get { return (string)GetValue(AmountProperty); }
+            set { SetValue(AmountProperty, value); }
+        }
+        public static readonly DependencyProperty AmountProperty =
+            DependencyProperty.Register("Amount", typeof(string), typeof(InfoCard));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(string), typeof(InfoCard));
     }
 }
